29a9ca769a0e8573424750770baf9b00799fc6ac
Vovaman <bd_postbox1@mail.ru>
2025-05-02 14:26:26 +0300

    #16 micropython v1.25.0 tested

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from machine import Pin
-import uasyncio as a
+import asyncio as a
 
 p2 = Pin(2, Pin.OUT)
 
 async def main():
     while True:
         await a.sleep_ms(200)
-        
+
         p2.on()
         await a.sleep_ms(1000)
         p2.off()
